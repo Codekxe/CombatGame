7f9d294677ee7f982d884e5a0fde47a469631b63
Codekxe <legiakhang23072011.thhungdao@gmail.com>
2024-01-25 21:10:48 +0700

    +more skeleton frames & improved knight hitbox

--- Assets/Scripts/KnightMovement.cs
+++ Assets/Scripts/KnightMovement.cs
@@ -24,7 +24,7 @@ public class KnightScript : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f)
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -48,130 +48,15 @@ public class KnightScript : MonoBehaviour
             anim.SetBool("IsWalking", false);
         }
 
-        if (rb.velocity.y > 0.01f)
+        if (rb.velocity.y > 0.001f)
         {
             anim.SetBool("IsJumping", true);
         }
 
-        if (rb.velocity.y < 0.01f)
+        if (rb.velocity.y < 0.001f)
         {
             anim.SetBool("IsJumping", false);
         }
     }
-    /*Animator anim;
-    float dirX, moveSpeed = 5.0f;
-    int healthPoints = 3;
-    bool isHurting, isDead, isAttacking;
-    bool facingRight = true;
-    Vector2 localScale;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        localScale = transform.localScale;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Jump") && !isDead && rb.velocity.y == 0)
-        {
-            rb.AddForce(Vector2.up * 600f);
-        }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveSpeed = 10f;
-        }
-        else
-        {
-            moveSpeed = 5f;
-        }
-
-        SetAnimationState();
-
-        if (!isDead)
-        {
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
-                anim.SetBool("isWalking", true);
-            }
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
-                anim.SetBool("isWalking", true);
-                localScale.x *= -1;
-                transform.localScale = localScale;
-            }
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if (!isHurting)
-            rb.velocity = new Vector2(dirX, rb.velocity.y);
-    }
-
-    void SetAnimationState()
-    {
-        if (dirX == 0)
-        {
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isRunning", false);
-        }
-
-        if (rb.velocity.y == 0)
-        {
-            anim.SetBool("isJumping", false);
-        }
-
-        if (Mathf.Abs(dirX) == 10 && rb.velocity.y == 0)
-            anim.SetBool("isRunning", true);
-        else
-            anim.SetBool("isRunning", false);
-
-        if (rb.velocity.y > 0)
-            anim.SetBool("isJumping", true);
-
-        if (rb.velocity.y < 0)
-        {
-            anim.SetBool("isJumping", false);
-        }
-    }
-
-   /* void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.name.Equals("Fire"))
-        {
-            healthPoints -= 1;
-        }
-
-        if (col.gameObject.name.Equals("Fire") && healthPoints > 0)
-        {
-            anim.SetTrigger("isHurting");
-            StartCoroutine("Hurt");
-        }
-        else
-        {
-            dirX = 0;
-            isDead = true;
-            anim.SetTrigger("isDead");
-        }
-    }
-
-    IEnumerator Hurt()
-    {
-        isHurting = true;
-        rb.velocity = Vector2.zero;
-
-        if (facingRight)
-            rb.AddForce(new Vector2(-200f, 200f));
-        else
-            rb.AddForce(new Vector2(200f, 200f));
-
-        yield return new WaitForSeconds(0.5f);
-
-        isHurting = false;
-    }*/
 }
+
